4d2e8534f002dfcd907e1b6e21dadc9ef4ddb2cc
Lupova19 <nadiqlupova@gmail.com>
2023-05-03 21:55:37 +0300

    first commit

---
+++ Phonebook/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Phonebook
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Vavedi comanda,contact, nomer ");
+            SortedDictionary<string, string> phonebook = new SortedDictionary<string, string>();
+
+            while (true)
+            {
+                string[] command = Console.ReadLine().Split();
+
+                if (command[0] == "A")
+                {
+                    string name = command[1];
+                    string phones = command[2];
+                    phonebook[name] = phones;
+                }
+                if (command[0] == "S")
+                {
+                    string name = command[1];
+                    if (phonebook.ContainsKey(name))
+                    {
+                        Console.WriteLine($"{name} -> {phonebook[name]}");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Kontacta {name} ne sashtestvuva.");
+                    }
+                }
+                else if (command[0] == "ListAll")
+                {
+                    Console.WriteLine("--------------------------------------------");
+
+                    foreach (var item in phonebook)
+                    {
+                        Console.WriteLine($"{item.Key} -> {item.Value}");
+                    }
+                }
+                else if (command[0] == "END")
+                {
+                    break;
+                }
+            }
+        }
+    }
+}
---
+++ RealNums/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RealNums
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Vavedi chisla: ");
+            double[] nums = Console.ReadLine().Split(' ').Select(double.Parse).ToArray();
+            SortedDictionary<double, int> sortedNums = new SortedDictionary<double, int>();
+
+            foreach (var item in nums)
+            {
+                if (sortedNums.ContainsKey(item))
+                {
+                    sortedNums[item]++;
+                }
+                else
+                {
+                    sortedNums[item] = 1;
+                }
+            }
+            foreach (var item in sortedNums.Keys) 
+            {
+                Console.WriteLine($"{item} -> {sortedNums[item]}");
+            }
+        }
+    }
+}
